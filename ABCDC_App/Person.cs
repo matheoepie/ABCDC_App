a84d083d7aa4e2adf44da9708545b606de1735e5
tompvr26 <tompoyvre@gmail.com>
2021-10-05 10:54:14 +0200

    add Program.cs + Person.cs + Session.cs

---
+++ ABCDC_App/Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABCDC_App
+{
+    class Person
+    {
+        public override string ToString()
+        {
+            return "NOM: " + LastName + "Prenom: " + FirstName;
+        }
+
+        #region Properties 
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public int Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public int BeginYear
+        {
+            get { return _beginYear; }
+            set { _beginYear = value; }
+        }
+
+        public string Weapon
+        {
+            get { return _weapon; }
+            set { _weapon = value; }
+        }
+
+        public string Armor
+        {
+            get { return _armor; }
+            set { _armor = value; }
+        }
+
+        #endregion
+
+        #region Attributes
+
+        private string _firstName;
+
+        private string _lastName;
+
+        private int _weight;
+
+        private int _beginYear;
+
+        private string _weapon;
+
+        private string _armor;
+
+        #endregion
+
+        public Person()
+        {
+
+        }
+    }
+}
---
+++ ABCDC_Test/ArmesFixesTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace ABCDC_Test
+{
+    class ArmesFixesTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Les_Armes_Sont_Fixes_Elles_Ne_Changent_Pas_Sur_Une_Session()
+        {
+            
+            //Assert.Equals();
+        }
+    }
+}
