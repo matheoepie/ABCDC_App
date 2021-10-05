c646ffbf5c5941361ebc4a8d9dea0d671a574a3c
EzHeisenberg <lucaporal@live.fr>
2021-10-05 10:54:43 +0200

    Test Add Excel with no methods

---
+++ ABCDC_App/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace ABCDC_App
+{
+    public class Person
+    {
+        #region Properties 
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public int Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public int BeginYear
+        {
+            get { return _beginYear; }
+            set { _beginYear = value; }
+        }
+
+        public string Weapon
+        {
+            get { return _weapon; }
+            set { _weapon = value; }
+        }
+
+        public string Armor
+        {
+            get { return _armor; }
+            set { _armor = value; }
+        }
+
+        #endregion
+
+        #region Attributes
+
+        private string _firstName;
+
+        private string _lastName;
+
+        private int _weight;
+
+        private int _beginYear;
+
+        private string _weapon;
+
+        private string _armor;
+
+        #endregion
+
+
+        public Person()
+        {
+
+        }
+    }
+}
