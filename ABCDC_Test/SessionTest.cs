3fed1f4dd8556e09099c2d026776045f6af6b28d
EzHeisenberg <lucaporal@live.fr>
2021-10-24 17:59:32 +0200

    creation du test un_joueur_quitte_la_session_entre_deux_parties

--- ABCDC_Test/SessionTest.cs
+++ ABCDC_Test/SessionTest.cs
@@ -26,13 +26,13 @@ namespace ABCDC_Test
             // GetListeExcel retourne la liste des personnes du fichier excel
             this.listJoueurSession = p.GetListeExcel();
 
-            // GetWeaponPlayerSession permet de reccuperer toutes les armes des joueurs dans la session
+            // GetWeaponPlayerSession permet de
+            // reccuperer toutes les armes des joueurs dans la session
             this.listWeaponSession = w.GetWeaponPlayerSession(this.listJoueurSession);
 
-            // GetArmorPlayerSession permet de reccuperer toutes les armures des joueurs dans la session
+            // GetArmorPlayerSession permet de
+            // reccuperer toutes les armures des joueurs dans la session
             this.listArmorSession = a.GetArmorPlayerSession(this.listJoueurSession);
-
-            
         }
 
 
@@ -51,6 +51,39 @@ namespace ABCDC_Test
 
             // Verifier que la permiere armure est bien egale a la premiere ligne du tableau excel
             Assert.AreEqual(session.armorsPlayerList[0].Name, "Mailles");
+
+        }
+
+
+        [Test]
+        public void un_joueur_quitte_la_session_entre_deux_parties()
+        {
+            // Creation de la session de jeu 
+            Session session = new Session() {
+                weaponsPlayerList = this.listWeaponSession,
+                armorsPlayerList = this.listArmorSession,
+                Date = DateTime.Now,
+                playerList = this.listJoueurSession,
+                partieList = new List<Partie>,
+            };
+
+            // Création d'une nouvelle partie
+            Partie p1 = new Partie(session.playerList);
+
+            // Ajouter la partie dans la liste des partie de la sessions
+            session.partieList.Add(p1);
+
+
+            // Suppréssion d'un joueur de la sessions
+            session.playerList.RemoveAll(x => x.LastName == "BLANC" && x.FirstName == "Louis");
+
+            // Création d'une seconde partie
+            Partie p2 = new Partie(session.playerList);
+            session.partieList.Add(p2);
+
+            // Verrification de la liste de joueur de la
+            // partie n°1 n'est pas egale a celle de la partie n°2
+            Assert.AreNotEqual(session.partieList[0].getPlayers(), session.partieList[1].getPlayers());
 
         }
 
